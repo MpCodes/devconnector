b79ec8f48ec041a6a9dbee08a0e3cfe0fde95a2a
abuuabdul <abuuabdul5@gmail.com>
2020-08-16 21:55:11 +0300

    get route for profile/me

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
+const {
+    check,
+    validationResult
+} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require("config");
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/auth
 // @desc    Test route
@@ -14,6 +21,74 @@ router.get('/', auth, async (req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
+    }
+});
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+
+//POST BUT VALIDATE KWANZA
+router.post('/', [check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()], async (req, res) => {
+    const errors = validationResult(req);
+    //return errors in an array
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    //destructuring i.e instead of using req.body.name === name
+    const {
+        email,
+        password
+    } = req.body;
+
+    // Handle logic issues
+    try {
+        // See if user exists
+        let user = await User.findOne({
+            email
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid Credentials'
+                }]
+            });
+        }
+
+        // Check if password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                errors: [{
+                    msg: 'Invalid Credentials'
+                }]
+            });
+        }
+
+        //generate payload for jwt
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        //sign the jwt payload then return a token after save
+        jwt.sign(payload, config.get("jwtSecret"), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err
+            res.json({
+                token
+            });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
     }
 });
 
