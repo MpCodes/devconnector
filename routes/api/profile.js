352fa4ca4c8086911ad26581129af30c4b5cb6ba
abuuabdul <abuuabdul5@gmail.com>
2020-09-08 01:18:11 +0300

    updated profile js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -115,4 +115,17 @@ router.post(
   }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles)
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
