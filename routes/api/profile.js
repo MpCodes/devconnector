9160bcbc0846e93bb3e2f64b0b1cda88f3022568
abuuabdul <abuuabdul5@gmail.com>
2020-09-07 06:57:51 +0300

    profile build

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,12 +15,12 @@ const User = require('../../models/User');
 router.get('/me', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({
-            user: req.user.id
+            user: req.user.id,
         }).populate('user', ['name', 'avatar']);
 
         if (!profile) {
             return res.status(400).json({
-                msg: 'There is no profile for this user'
+                msg: 'There is no profile for this user',
             });
         }
 
@@ -34,9 +34,55 @@ router.get('/me', auth, async (req, res) => {
 // @route   GET api/profile
 // @desc    Create or update user profile
 // @access  Private
-router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), check('skills', 'Skills is required').not().isEmpty()]], (req, res) => {
+router.post(
+    '/',
+    [
+        auth,
+        [
+            check('status', 'Status is required').not().isEmpty(),
+            check('skills', 'Skills is required').not().isEmpty(),
+        ],
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
 
-});
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
 
+        // Build profile object
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if (company) profileFields.company = company;
+        if (website) profileFields.website = website;
+        if (location) profileFields.location = location;
+        if (bio) profileFields.bio = bio;
+        if (status) profileFields.status = status;
+        if (githubusername) profileFields.githubusername = githubusername;
+        if (skills) {
+            profileFields.skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        // Build social object
+        profileFields.social = {};
+
+    }
+);
 
 module.exports = router;
