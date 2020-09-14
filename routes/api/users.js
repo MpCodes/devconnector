1afe13debef75440e93e1c3034cb28e76be98075
twiga <abubakar@twigahosting.co.tz>
2020-09-14 18:24:31 +0300

    changed uppercase user to lower case

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const User = require('../../models/User');
 const router = express.Router();
 const {
 	check,
 	validationResult
 } = require('express-validator');
-
-const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
