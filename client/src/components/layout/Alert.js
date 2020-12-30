732b9c1fd40a6f5a44a2aadf6daa3531a84e009d
twiga <abubakar@twigahosting.co.tz>
2020-12-30 18:54:16 +0300

    redux authentication

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,9 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const Alert = ({ alerts }) => {
-  return <div></div>;
-};
+const Alert = ({ alerts }) =>
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
+  ));
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired,
@@ -14,4 +19,4 @@ const mapStateToProps = (state) => ({
   alerts: state.alert,
 });
 
-export default connect()(Alert);
+export default connect(mapStateToProps)(Alert);
